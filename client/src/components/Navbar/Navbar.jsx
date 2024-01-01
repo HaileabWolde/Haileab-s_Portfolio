79ac6cd0b3e56008cd02c7807251cb30b269e7dd
Haileab <womahaileab1428@gmail.com>
2024-01-01 14:38:28 +0300

    added a css for navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './Navbar.scss'
-
+import {images} from '../../constants'
 const Navbar = ()=> {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo}/>
       </div>
+      <ul className='app__navbar-links'>
+        {
+          ['home', 'about', 'works', 'skills', 'contact'].map((item)=> (
+            <li key={`link-${item}`} className=' p-text'>
+              <div/>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))
+        }
+      </ul>
+    </nav>
   )
 }
 
