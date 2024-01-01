427f7937295139be900019287a717021d2e4b05f
Haileab <womahaileab1428@gmail.com>
2024-01-01 20:24:27 +0300

    added some animation to it

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {HiMenuAlt4, HiX} from 'react-icons/hi'
+import {motion} from 'framer-motion'
 import './Navbar.scss'
 import {images} from '../../constants'
+
 const Navbar = ()=> {
+  const [toggle, setToggle] = useState(false)
+  console.log(toggle)
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.logo}/>
+        <img src={images.logo} alt='image name'/>
       </div>
       <ul className='app__navbar-links'>
         {
@@ -17,6 +22,27 @@ const Navbar = ()=> {
           ))
         }
       </ul>
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={()=> setToggle(true)}/>
+        {
+          toggle && (
+            <motion.div
+              whileInView={{x:[300, 0]}}
+              transition={{duration: 0.85, ease: 'easeInOut'}}
+            >
+               
+        {
+          ['Home', 'About', 'Works', 'Skills', 'Contact'].map((item)=> (
+            <li key={`${item}`} >
+              <a href={`#${item}`} onClick={()=> setToggle(false)}>{item}</a>
+            </li>
+          ))
+        }
+      
+            </motion.div>
+          )
+        }
+      </div>
     </nav>
   )
 }
