17076de6abb64a70ac465743abc713f73b622383
Haileab <womahaileab1428@gmail.com>
2024-01-10 00:00:04 +0300

    finshing my portfolio

--- client/src/container/Footer/Footer.jsx
+++ client/src/container/Footer/Footer.jsx
@@ -2,7 +2,10 @@ import React, {useState} from 'react'
 import { images } from '../../constants'
 import './Footer.scss'
 import { AppWrapper, Motionwrapper } from '../../wrapper'
+import {client } from '../../client'
 const Footer = ()=>{
+    const [isloading, setLoading] = useState(false)
+    const [fillForm, setForm] = useState(false)
     const [FormData, setFormData] = useState({
         Name: '',
         Email: '',
@@ -16,7 +19,21 @@ const Footer = ()=>{
             }
         )
     }
-    console.log(FormData)
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    setLoading(true);
+    const contact = {
+        _type: 'contact',
+        name: FormData.Name,
+        email: FormData.Email,
+        message: FormData.Messages
+    }
+    client.create(contact).
+    then(()=> {
+        setLoading(false)
+        setForm(true)
+    })
+  }
     return (
         <div  className="MAIN" style={{backgroundColor: '#fff'}}>
             <h1 className='head-text'>Contact with <span> me <span></span></span></h1>
@@ -30,43 +47,54 @@ const Footer = ()=>{
                     <a href="tel:0992621400" className="p-text">0992621400</a>
                 </div>
             </div>
-            <form className='InputField'>
-                <div>
-                  <input
-                  placeholder='Your Name'
-                  type='text'
-                  name='Name'
-                  value={FormData.Name}
-                  onChange={(e)=> handleChange(e)}
-                  />
-                </div>
-                <div>
-                <input
-                  placeholder='Your Email'
-                  type='email'
-                  name='Email'
-                  value={FormData.Email}
-                  onChange={(e)=> handleChange(e)}
-                  /> 
-                </div>
-                <div>
-                <textarea
-                  placeholder='Your Messages'
-                  type='text'
-                  name='Messages'
-                  value={FormData.Messages}
-                  onChange={(e)=> handleChange(e)}
-                  /> 
-                </div>
-                <button>
-                    Send Message
-                </button>
-            </form>
+            {
+                fillForm ?  (
+                    <h4 className='head-text '>Thank You For Your Message</h4>
+                 )
+                 : (
+                    <form className='InputField' onSubmit={handleSubmit}>
+               
+                    <div>
+                      <input
+                      placeholder='Your Name'
+                      type='text'
+                      name='Name'
+                      value={FormData.Name}
+                      onChange={(e)=> handleChange(e)}
+                      />
+                    </div>
+                    <div>
+                    <input
+                      placeholder='Your Email'
+                      type='email'
+                      name='Email'
+                      value={FormData.Email}
+                      onChange={(e)=> handleChange(e)}
+                      /> 
+                    </div>
+                    <div>
+                    <textarea
+                      placeholder='Your Messages'
+                      type='text'
+                      name='Messages'
+                      value={FormData.Messages}
+                      onChange={(e)=> handleChange(e)}
+                      /> 
+                    </div>
+                    <button type='submit'>
+                       {
+                        isloading ? 'Sending Message ...' : 'Send Message'
+                       }
+                    </button>
+                </form>
+                ) 
+            }
+           
            
         </div>
     )
 }
 export default AppWrapper(
     Motionwrapper(Footer, 'aboutWork'),
-    'contact', 'aboutWork'
+    'contact', 'app__container_1 '
 )
